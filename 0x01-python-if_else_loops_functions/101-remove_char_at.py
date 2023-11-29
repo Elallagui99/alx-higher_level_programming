b95b121e0faa6f812ce63aaf83f47100714222d1
Elallagui99 <got.famme@gmail.com>
2023-11-30 00:47:27 +0100

    remove char at giving index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    for i in range(str):
-        if i != n:
-            str1 = str[i]
-    print("{}".format(str1), end="")
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
