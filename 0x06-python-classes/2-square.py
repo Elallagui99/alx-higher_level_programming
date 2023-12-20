fe1ba5c08b52122d469eaba68115ebd9b9bdca8c
Elallagui99 <got.famme@gmail.com>
2023-12-20 01:10:28 +0100

    class with instance exception

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+
+
+class Square:
+    """non empty square class"""
+
+    def __init__(self, size=0):
+        """initialize a new square
+        Args:
+        size(int): private size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
