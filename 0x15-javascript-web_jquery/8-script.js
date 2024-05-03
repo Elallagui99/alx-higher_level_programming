188e3a4223098f311b66ebbbbcc1163dbacd0007
Elallagui99 <got.famme@gmail.com>
2024-05-03 03:33:34 +0100

    add

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  data.results.forEach(function (movie) {
+    $('<li>').text(movie.title).appendTo('#list_movies');
+  });
+});
